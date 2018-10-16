d293c151c91c10cf26d37c278661611bce2a6d3e
sbx_user1060 <sbx_user1060@ip-10-129-121-53.eu-west-2.compute.internal>
2018-10-16 16:42:40 +0000

    Tue Oct 16 16:42:40 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -4,10 +4,7 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class SumSolution {
 
-    public int compute(int x, int y) throws Exception {
-    	if(x<0 || x > 100 || y > 0 || y > 100){
-    		throw new Exception("x or y are out of bounds");
-    	}
+    public int compute(int x, int y) {
     	return x+y;
     }
 
