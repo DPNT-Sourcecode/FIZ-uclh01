0d1ce6f6eda4c474212d95875ae977820f898f41
sbx_user1060 <sbx_user1060@ip-10-129-121-53.eu-west-2.compute.internal>
2018-10-16 17:15:14 +0000

    Tue Oct 16 17:15:14 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -19,6 +19,7 @@ public class FizzBuzzSolution {
         	int c = number % 10;
         	int copy = number/10;
         	boolean deluxe = true;
+        	boolean fakeDeluxe = false;
         	while(copy > 0){
         		if(copy % 10 != c){
         			deluxe = false;
@@ -27,16 +28,23 @@ public class FizzBuzzSolution {
         		copy = copy / 10;
         	}
         	if(deluxe == true && print == "" && number % 2 == 1){
-        		print = print + "fake deluxe";
+        		fakeDeluxe = true;
+        		deluxe = false;
+        		print = "fake deluxe";
         	}else if (deluxe == true && print == "" && number % 2 == 0){
-        		print = print + "deluxe";
+        		print = "deluxe";
         	}else if (deluxe == true && print.equals("fizz buzz")){
-        		print = print + " deluxe";
+        		print = "fizz buzz deluxe";
         	}else if (deluxe == true && print.equals("fizz")){
         		print = "fizz deluxe";
         	}else if (deluxe == true && print.equals("buzz")){
         		print = "buzz deluxe";
-        	}
+        	}else if (fakeDeluxe == true && print.equals("fizz buzz")){
+        		print = "fizz buzz fake deluxe";
+        	}else if (fakeDeluxe == true && print.equals("fizz")){
+        		print = "fizz fake deluxe";
+        	}else if (fakeDeluxe == true && print.equals("buzz")){
+        		print = "buzz fake deluxe";
         }
         if(print == "")
         	return nr;
